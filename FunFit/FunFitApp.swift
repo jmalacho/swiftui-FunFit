5c85c11ea700a418b3c207e49a9b4ab8c25d2ba1
Jon Malachowski <jmalacho@gmail.com>
2025-03-16 11:36:15 -0400

    Initial Commit

---
+++ FunFit/FunFitApp.swift
@@ -0,0 +1,32 @@
+//
+//  FunFitApp.swift
+//  FunFit
+//
+//  Created by Jon Malachowski on 3/16/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FunFitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FunFitTests/FunFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  FunFitTests.swift
+//  FunFitTests
+//
+//  Created by Jon Malachowski on 3/16/25.
+//
+
+import Testing
+@testable import FunFit
+
+struct FunFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
